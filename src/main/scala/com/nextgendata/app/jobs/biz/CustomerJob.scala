e364b54066ec3ca49b319fdca9789595d1cb87a2
jamalraj <jamalraj.learn@gmail.com>
2016-05-25 08:59:12 -0400

    Partial Commit... Working on it with Shared variables and accumulator.

--- src/main/scala/com/nextgendata/app/jobs/biz/CustomerJob.scala
+++ src/main/scala/com/nextgendata/app/jobs/biz/CustomerJob.scala
@@ -18,7 +18,12 @@ object CustomerJob extends Job {
     // this is used to implicitly convert an RDD to a DataFrame or Dataset.
     import sqlContext.implicits._
 
+    val rowInsertCnt = Job.sc.accumulator(0,"Customer Insert")
+    val rowSkipCnt = Job.sc.accumulator(0,"Customer Skip")
+
     val customers = SrcBizCustomer.getCustomersWithCif
+
+    //val countryMap = CountryCodeMapTbl.getCountryCodeMap
 
     val dflInvLogPcm = new ProvinceCodeMap(ProvinceCodeMap(sqlContext))
       with Logging[ProvinceCodeMapKey, ProvinceCodeMapVal] with StdOutLogging[ProvinceCodeMapKey, ProvinceCodeMapVal]
@@ -28,25 +33,41 @@ object CustomerJob extends Job {
       with Logging[CountryCodeMapKey, CountryCodeMapVal] with StdOutLogging[CountryCodeMapKey, CountryCodeMapVal]
       with ApplyDefaultInvalid[CountryCodeMapKey, CountryCodeMapVal]
 
-
     val trgCustomerRows = customers.map(srcCust => {
-      //Mapping the tuple to named values instead of using _1, _2, _3 syntax
+        //Mapping the tuple to named values instead of using _1, _2, _3 syntax
       val (bizCustRow, cifXrefRow, cifCustRow) = srcCust
 
       val provCd = dflInvLogPcm.get(ProvinceCodeMapKey(bizCustRow.province)).getOrElse(dflInvLogPcm.getDefault)
 
       val cntryCd = dflInvLogCntrPcm.get(CountryCodeMapKey(provCd.countryCd)).getOrElse(dflInvLogCntrPcm.getDefault)
 
-        TrgCustomerRow(
-          bizCustRow.email,
-          provCd.provinceCd,
-          provCd.provinceName,
-          cntryCd.countryName,
-          bizCustRow.postal,
-          cifCustRow.CIFId
-        )
-    })
+      val countryRdd = Job.sc.parallelize(Seq(cntryCd))
+      val broadcastedCntry = Job.sc.broadcast(cntryCd)
 
+      if (provCd.provinceName == "-99" ||
+        broadcastedCntry.value.countryName == "-99") {
+        rowSkipCnt += 1
+        //throw new InvalidRowException("Not Valid")
+      }
+      else {
+        rowInsertCnt += 1
+      }
+      TrgCustomerRow(
+        bizCustRow.email,
+        provCd.provinceCd,
+        provCd.provinceName,
+        broadcastedCntry.value.countryName,
+        bizCustRow.postal,
+        cifCustRow.CIFId
+      )
+    })
     TrgCustomer.insert(trgCustomerRows)
+    println("""Skipped Row Count """ + rowSkipCnt)
+    println("""Inserted Row Count """ + rowInsertCnt)
   }
 }
+
+class InvalidRowException(smth: String) extends Exception(smth) {
+
+}
+
